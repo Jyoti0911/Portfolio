de0b23ae60c15ecdf42306aca1c1bba176f56334
Jyoti kumari <jyotikumari091199@gmail.com>
2022-01-17 23:14:25 +0530

    editing nav

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
@@ -22,7 +22,7 @@ menu_item.forEach((item) => {
 		hamburger.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
 	});
-});*/
+});
  // typing text animation script
  var typed = new Typed(".typing", {
 	strings: [ "Software Developer", "Web Developer", "Frontend Developer"],
